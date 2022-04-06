8a79d21345ad094dd0caa291cbc7eb15974d5eef
EduardoMoraes <17eduardo05@gmail.com>
2022-04-05 22:41:20 -0300

    main

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {Text, TouchableOpacity, TextInput, View, StyleSheet, StatusBar} from 'react-native';
+import * as Speech from 'expo-speech'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import meuestilo from './meuestilo';
+
+export default class App extends React.Component{
+  state={
+    valor1:0.0,
+    valor2:0.0,
+    resultado:0.0,
+  }
+
+  atualizaValor1=(number)=>{
+    this.setState({valor1:number})
+  }
+  atualizaValor2=(number)=>{
+    this.setState({valor2:number})
+  }
+   atualizaResultado=(number)=>{
+    this.setState({resultado:number})
+  }
+
+  soma(){
+    this.state.resultado=parseFloat(this.state.valor1)+parseFloat(this.state.valor2);
+    Speech.speak("o resultado dessa soma é"+this.state.resultado)
+    alert("O Resultado é: "+this.state.resultado);
+  }
+
+  multi(){
+    this.state.resultado=parseFloat(this.state.valor1)*parseFloat(this.state.valor2);
+    Speech.speak("o resultado dessa multiplicação é"+this.state.resultado)
+    alert("O Resultado é: "+this.state.resultado);
+  }
+
+  division(){
+    this.state.resultado=parseFloat(this.state.valor1)/parseFloat(this.state.valor2);
+    Speech.speak("o resultado dessa divisão é"+this.state.resultado)
+    alert("O Resultado é: "+this.state.resultado);
+  }
+
+  subtra(){
+     this.state.resultado=parseFloat(this.state.valor1)-parseFloat(this.state.valor2);
+     Speech.speak("o resultado dessa subtração é"+this.state.resultado)
+     alert("O Resultado é: "+this.state.resultado);
+  }
+
+  render(){
+    return(
+      <View style={meuestilo.container}>
+      
+        <StatusBar backgroundColor="black" style={meuestilo.fontegrande}/>
+        <TextInput underlineColorAndroid="transparent" 
+        placeholder="digite valor 1" 
+        placeholderTextColor="black"
+        autoCapitalize='none'
+        onChangeText={this.atualizaValor1}
+        keyboardType='numeric'
+        style={meuestilo.input} />
+
+        <TextInput underlineColorAndroid="transparent" 
+        placeholder="digite valor 2" 
+        placeholderTextColor="black"
+        autoCapitalize='none'
+        onChangeText={this.atualizaValor2}
+        keyboardType='numeric' 
+        style={meuestilo.input}/>
+
+        <TouchableOpacity onPress={()=>this.soma(this.state.valor1, this.state.valor2, this.state.resultado)} style={meuestilo.submitButton}>
+          <Text style={meuestilo.submitButtonText}>Somar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.multi(this.state.valor1, this.state.valor2, this.state.resultado)} style={meuestilo.submitButton}>
+          <Text style={meuestilo.submitButtonText}>Multiplicar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.division(this.state.valor1, this.state.valor2, this.state.resultado)} style={meuestilo.submitButton}>
+          <Text style={meuestilo.submitButtonText}>Divisão</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity onPress={()=>this.subtra(this.state.valor1, this.state.valor2, this.state.resultado)} style={meuestilo.submitButton}>
+          <Text style={meuestilo.submitButtonText}>Subtrair</Text>
+        </TouchableOpacity>
+        
+        </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
